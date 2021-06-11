cb66156d231c790c4260c793951c1b6e0b42c10d
Austin Mckee <captainlantern8@gmail.com>
2021-06-10 17:24:59 -0700

    linkedin

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,3 +1,4 @@
+import Linkedin from "./photos/Linkedin.png";
 export default function Contact() {
 	return (
 		<section className="container mt-5 " id="contact">
@@ -5,11 +6,7 @@ export default function Contact() {
 				<article className="text-box ">
 					<h2>Contact</h2>
 					<div className="row">
-						<form
-							action="https://formspree.io/f/mqkgorlk"
-						
-							method="POST"
-						>
+						<form action="https://formspree.io/f/mqkgorlk" method="POST">
 							<label for="user-name">Name:</label>
 							<input
 								id="user-name"
@@ -19,12 +16,31 @@ export default function Contact() {
 							/>
 
 							<label for="user-email">Email:</label>
-							<input 	className="form-control" id="user-email" type="text" name="email" />
+							<input
+								className="form-control"
+								id="user-email"
+								type="text"
+								name="email"
+							/>
 
 							<label for="user-message">Your message:</label>
-							<textarea 	className="form-control" id="user-message" name="message"></textarea>
-							<button	className="btn btn-success" type="submit">Submit</button>
+							<textarea
+								className="form-control"
+								id="user-message"
+								name="message"
+							></textarea>
+							<button className="btn btn-success" type="submit">
+								Submit
+							</button>
 						</form>
+					</div>
+					<div className="mt-3">
+						<a
+							href="https://www.linkedin.com/in/the-austin-mckee/"
+							target="_blank"
+						>
+							<img src={Linkedin} width="64px"></img>
+						</a>
 					</div>
 				</article>
 			</div>
